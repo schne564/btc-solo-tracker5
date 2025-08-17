c6a2a5bba8f33d41e68d4bb2419c3eb59e4207b6
schne564 <Schne564@gmail.com>
2025-08-17 16:58:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ function updateStats(address) {
       document.getElementById("timeEstimate").textContent = data.timeEstimate;
 
    //   document.getElementById("lastUpdated").textContent = "Last updated: " + new Date().toLocaleTimeString();
-    })
-    .catch((err) => {
-      console.error("Error fetching data:", err);
-      document.getElementById("lastUpdated").textContent = "Error fetching data";
+  //  })
+ //   .catch((err) => {
+   //   console.error("Error fetching data:", err);
+     // document.getElementById("lastUpdated").textContent = "Error fetching data";
     });
 }
 
